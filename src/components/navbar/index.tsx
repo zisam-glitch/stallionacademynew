37faffe4b244ced0b6a2e76f9ea44db6c7367646
zisam shaikh <zisamshaikh7@gmail.com>
2024-10-06 02:41:59 +0600

    revision 6

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,10 +2,10 @@ import Header from "./header";
 import Topbar from "./topbar";
 export default function Index() {
   return (
-    <>
-      {" "}
+    <section>
+    
       <Topbar />
       <Header />
-    </>
+    </section>
   );
 }
